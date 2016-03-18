ddaa5b3349902e060d7e86eaabd2cf58902914fa
Roman Ivanov <ivanov-jr@mail.ru>
2016-03-18 06:28:47 -0700

    minor: update for fastforward shell file

--- fast-forward-merge.sh
+++ fast-forward-merge.sh
@@ -1,37 +1,43 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+
+# GitHub does not support fast-forward merge.
+# This script is intended to simplify merge of Pull Requests to keep history linear (fast-forwarded)
+# All Pull Requests from GitHub to our project has to be applied to our code by this script.
+
+set -e
 
 if [ $# -eq 0 ]
   then
-    echo "$(basename "$0") GIT_REPO FORK_USER_NAME USER_BRANCH
+    echo "$(basename "$0") FORK_USER_NAME USER_BRANCH
 example:
 
-    ./$(basename "$0") sevntu.checkstyle konstantinos issue73
+    ./$(basename "$0") konstantinos issue73
 "
-	exit 0;
+    exit 0;
 fi
 
-GIT_REPO=$1
-FORK_USER_NAME=$2
-USER_BRANCH=$3
-REPO=$FORK_USER_NAME-fork
-LOCAL_USER_BRANCH=$FORK_USER_NAME-$USER_BRANCH
+GIT_REPO=dsm-maven-plugin
+FORK_USER_NAME=$1
+USER_BRANCH=$2
+REPO=${FORK_USER_NAME}-fork
+LOCAL_USER_BRANCH=${FORK_USER_NAME}-${USER_BRANCH}
 
 echo "adding remote ..."
-git remote add $REPO https://github.com/$FORK_USER_NAME/$GIT_REPO.git
-git fetch $REPO
+git remote add ${REPO} https://github.com/${FORK_USER_NAME}/${GIT_REPO}.git
+git fetch ${REPO}
 
 echo "creating local branch ..."
-git checkout -b $LOCAL_USER_BRANCH $REPO/$USER_BRANCH
+git checkout -b ${LOCAL_USER_BRANCH} ${REPO}/${USER_BRANCH}
 
 echo "rebasing over master ..."
 git rebase master
 
 echo "merge to master ..."
 git checkout master
-git merge $LOCAL_USER_BRANCH
+git merge ${LOCAL_USER_BRANCH} --ff-only
 
 echo "removing local branch ..."
-git branch -D $LOCAL_USER_BRANCH
+git branch -D ${LOCAL_USER_BRANCH}
 
 echo "removing remote ..."
-git remote rm $REPO
+git remote rm ${REPO}
